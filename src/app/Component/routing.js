524d598b36d226b8ea399e43da24231811756a1b
raghuvaran-cpu <raghuvaran@gmail.com>
2021-08-12 15:18:48 +0530

    pushed changes

--- src/app/Component/routing.js
+++ src/app/Component/routing.js
@@ -2,9 +2,6 @@ import React from "react";
 import Createaccount from "./registration";
 import ChatScreen from "./whtsapp/chatscreen";
 import ChatRoom from "./chatroom/ChatRoom";
-import ProfileUser from "./HamBurgerMenu/profile";
-import Themes from "./HamBurgerMenu/themes";
-import Help from "./HamBurgerMenu/help";
 import Register from "./RegisterUser/RegisterUser";
 import Login from './Login/Login';
 import {
@@ -28,9 +25,6 @@ export default class Routing extends React.Component {
                     <Route path='/' exact component={Createaccount }></Route>
                     <Route path='/ChatRoom' exact component={ChatRoom}/>
                     <Route path='/chats' exact component={ChatScreen}></Route>
-                    <Route path='/profile' exact component={ProfileUser}></Route>
-                    <Route path='/themes' exact component={Themes}></Route>
-                    <Route path='/help' exact component={Help}></Route>
                     <Route path='/register' exact component={Register}></Route>
                     <Route path='/login' exact component={Login} />
                     <Route path='*' exact component={PageNotFound}></Route>
--- src/app/Component/whtsapp/chatscreen.js
+++ src/app/Component/whtsapp/chatscreen.js
@@ -1,70 +1,62 @@
 import React, { Component } from 'react';
 import './chatscreen.css';
-import { socketConnect } from '../../../service/socket';
-
+import ChatRoom from '../chatroom/ChatRoom';
 export default class ChatScreen extends Component {
-
     constructor(props) {
         super(props)
         this.state = {
             Data: null,
-            isLoading: true,
+            isLoading:true,
             user: this.props.location.state && this.props.location.state.user,
-            menu: false,
-            settingDetails: false,
+            menu:false,
+            settingDetails:false,
         }
         console.log(this.props);
     }
-
     componentDidMount() {
-        socketConnect((socket) => {
-            this.socket = socket;
-            this.getContacts();
-        });
+        console.log(this.state.Data);
+        this.getContacts();
     }
-
-    getContacts = () => {
-        fetch("https://ptchatindia.herokuapp.com/contacts").then(res => res.json()).then(res => {
-            console.log("response", res);
-            let details = [];
-            res.map((user, index) => {
-                if (user.username === this.state.user) {
-                    this.setState({ user: user });
+    getContacts=()=>{
+        //https://ptchatindia.herokuapp.com/contacts
+        fetch("https://ptchatindia.herokuapp.com/contacts").then(res => res.json()).then(res=>{
+            console.log("response",res);
+            let index=null,details=[];
+            res.map((user,index)=>{
+                
+                if(user.username === this.state.user){
+                    this.setState({user:user});
+                    index=index;
+                    
                 }
-                else {
+                else{
                     details.push(user);
-                    this.socket.emit("joinRoom", { username: this.state.user, client2: user.username });
                 }
-            });
-            this.setState({ Data: details, isLoading: false });
+            })
+            this.setState({Data:details,isLoading:false});
         })
     }
-
-    open = (user) => {
+    open=(user)=> {
         this.props.history.push({
-            pathname: '/ChatRoom',
+            pathname:'/ChatRoom',
             userDetails: this.state.user,
             client2: user
         })
     }
-
-    settings = () => {
-        this.setState({ menu: true })
+    settings=()=>{
+        this.setState({menu:true})
     }
-
-    settingDetails = () => {
-        this.setState({ settingDetails: true })
+    settingDetails=()=>{
+        this.setState({settingDetails:true})
     }
-
-    cancel = () => {
-        this.setState({ menu: false, settingDetails: false })
+    cancel=()=>{
+        this.setState({menu:false,settingDetails:false})
     }
-
     render() {
-        const { isLoading, Data } = this.state;
+        const {isLoading,Data}=this.state;
         console.log(Data);
-        if (isLoading) {
-            return (
+        if(isLoading){
+            return(
                 <div>
                     Loding...
                 </div>
@@ -72,54 +64,58 @@ export default class ChatScreen extends Component {
         }
         return (
             <div className="entire-area">
-
-                <div className="header">
-                    <div className="headings"><h1>Chats</h1></div>
-                    <div>
-                        {this.state.menu ?
-                            this.state.settingDetails ?
-                                <div className="screen-pop-up">
-                                    <div className="settings-details-header">
-                                        <button onClick={() => { this.cancel() }}>X</button>
-                                        <h1 style={{ color: 'white' }}>About</h1>
-                                    </div>
-                                    <div className="settings-details-body">
-                                        <img src="" />
-                                        <span>name</span>
-                                    </div>
+                
+            <div className="header">
+                <div className="headings"><h1>Chats</h1></div>
+                <div>
+                    {this.state.menu?
+                        this.state.settingDetails?
+                            <div className="screen-pop-up-profile">
+                                <div className="settings-details-header">
+                                    <h1 style={{color:'white'}}>About</h1>
+                                    <span><button className="settings-details-cancel" onClick={()=>{this.cancel()}}>X</button></span>
                                 </div>
-                                :
-
-                                <div className="screen-pop-up">
-                                    <div onClick={() => { this.settingDetails() }} style={{ padding: 10, paddingRight: 40, cursor: 'pointer' }} > <img src="https://cdn3.vectorstock.com/i/1000x1000/08/37/profile-icon-male-user-person-avatar-symbol-vector-20910837.jpg" style={{ width: 20, height: 20 }} /> Profile</div>
-                                    <div style={{ padding: 10, paddingRight: 30, cursor: 'pointer' }} ><img src="https://static.vecteezy.com/system/resources/thumbnails/001/500/478/small/theme-icon-free-vector.jpg" style={{ width: 20, height: 20 }} />Themes</div>
-                                    <div style={{ padding: 10, paddingRight: 50, cursor: 'pointer' }} ><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbGk-AsWSk4bsvhARgxG4RxWrx41LLfscW1g&usqp=CAU" style={{ width: 20, height: 20 }} /> Help</div>
+                                <div className="settings-details-body">
+                                    <span><img className="settings-profile-image" src={this.state.user.profile} /></span>
+                                    <span className="settings-profile-text"><h5>{this.state.user.username}</h5></span>
                                 </div>
-
-                            :
-                            <div style={{ borderRadius: 50 }} >
-                                <button className="screen-menu" onClick={() => { this.settings() }}><h2>⋮</h2></button>
+                                <div className="settings-details-footer">
+                                    <span className="settings-profile-text"><h5>Email : </h5>{this.state.user.email}</span>
+                                    <span className="settings-profile-text"><h5>Phone : </h5>{this.state.user.mobile}</span>
+                                </div>
                             </div>
-                        }
-                    </div>
-                </div>
-                <div style={{ backgroundColor: this.state.color }}>
-                    <div className='chats'>
+                        :
 
-                        {this.state.Data.map((user, index) => {
-                            return (
-                                <div key={index} className='contact'>
-                                    <div className='profile-img'>
-                                        <img src={user.profile} className="image"></img>
-                                    </div>
-                                    <div className="text profile-nm">
-                                        <h2 onClick={() => { this.open(user) }}>{user.username}</h2>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
+                        <div className="screen-pop-up">
+                            <div className="screen-pop-up-heading srn-head" onClick={()=>{this.settingDetails()}}>  Profile</div>
+                            <div className="screen-pop-up-delete-user srn-delete">delete user</div>
+                            <div className="screen-pop-up-archieve srn-archieve">Add to archieve</div>
+                            <div className="screen-pop-up-block srn-block"> block</div>
+                        </div>
+                        
+                    :
+                        <div >
+                            <button className="screen-menu" onClick={()=>{this.settings()}}>...</button>
+                        </div>
+                    }
                 </div>
+            </div>
+            <div style={{backgroundColor:this.state.color}}>
+            <div className='chats'>
+                
+                {this.state.Data.map((user,index) => {
+                    return(
+                    <div key={index} className='contact'>
+                    <div className='profile-img'>
+                        <img src={user.profile} className="image"></img>
+                        </div>
+                       <div className="text profile-nm">
+                        <h2 onClick={()=>{this.open(user)}}>{user.username}</h2>
+                        </div>
+                    </div>
+                )})}
+            </div>
+            </div>
             </div>
         )
     }
