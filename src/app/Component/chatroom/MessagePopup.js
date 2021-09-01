90421a84eeb2e49aa5e46f76b28846de5ddb8327
Sagar11-19 <sagar.yellaram@xcubelabs.com>
2021-09-01 18:54:04 +0530

    'DeleteMessage'

--- src/app/Component/chatroom/MessagePopup.js
+++ src/app/Component/chatroom/MessagePopup.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
 import './chatroom.css'
 export default class MessagePopup extends Component {
+
+
+   deleteMessage=(msgid)=>
+   {
+
+       this.props.socket.emit("delete", { username:this.props.userName, client:this.props.clientName, messageId:msgid })
+
+
+   }
+
+
     render() {
         
             return (
@@ -8,7 +19,7 @@ export default class MessagePopup extends Component {
                     <div className={(this.props.type==="right")? 'messagepopup-right': 'messagepopup-left'} >
                     <div className="messagepopup-items" >Reply</div>
                            <div className="messagepopup-items">Forward Message</div>
-                           <div className="messagepopup-items">Delete Message</div>
+                           <div className="messagepopup-items"   onClick={()=>this.props.type==='right' && this.deleteMessage(this.props.messageId)} >Delete Message</div>
                            <div className="messagepopup-items" >Star Messages</div>
                 </div>
                 </div>
